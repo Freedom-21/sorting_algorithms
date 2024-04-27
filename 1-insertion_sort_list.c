46410ba84702ce48b6c398e1e8f284a7c532f37e
Freedom-21 <yenesewfreedom8@gmail.com>
2024-04-27 02:15:43 +0200

    updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,31 +1,39 @@
 #include "sort.h"
 
 /**
- * create_listint - Creates a doubly linked list from an array of integers
- *
- * @array: Array to convert to a doubly linked list
- * @size: Size of the array
- *
- * Return: Pointer to the first element of the created list. NULL on failure
+ * insertion_sort_list - sorts a doubly linked list of integers in asc order
+ * using the Insertion Sort algorithm
+ * @list: Double pointer to the head of the doubly linked list
  */
 
-listint_t *create_listint(const int *array, size_t size)
+void insertion_sort_list(listint_t **list)
 {
-	listint_t *list = NULL;
-	listint_t *node;
+	listint_t *current, *tmp;
 
-	while (size--)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
 	{
-		node = malloc(sizeof(*node));
-		if (!node)
-			return (NULL);
-		node->n = array[size];
-		node->next = list;
-		node->prev = NULL;
+		tmp = current;
+		current = current->next;
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
 
-		if (list)
-			list->prev = node;
-		list = node;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+
+		if (tmp->prev != NULL)
+			tmp->prev->next = tmp;
+		else
+			*list = tmp;
+
+		print_list(*list);
+		}
 	}
-	return (list);
 }
